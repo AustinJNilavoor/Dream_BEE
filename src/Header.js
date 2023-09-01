621cd1f76edbebefdd7e4dae059b55cd382d895c
lordtransyl <dillsagar@gmail.com>
2023-09-01 19:25:49 +0530

    Navbar added

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,13 @@
 const Header = () => {
     return(
         <><nav>
+            <div>
             <ul className="nav_links">
                 <li><a href="Home.js">Home</a></li>
                 <li><a href="products.js">Products</a></li>
                 <li><a href="cart.js">Cart</a></li>
                 <li><a href="footer.js">Contact</a></li>
-            </ul>
+            </ul></div>
         </nav></>
     );
 }
