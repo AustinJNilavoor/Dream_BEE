92f1fcefc59a630d2938aec7b2c69c5cb80b8370
Austin J Nilavoor <nilavooraustin@gmail.com>
2023-09-06 20:21:19 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './elements.css';
-import React, { useState } from "react";
 import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Footer from './footer';
 import Products from './products';
 function App() {
-  const [navbarOpen, setNavbarOpen] = useState(false)
   return (
     <div className="App">
       <div className="bgimage"></div>
       <header className="App-header">
-        <Header navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen}/>
+        <Header/>
       </header>
       <Home />
       <Products />
